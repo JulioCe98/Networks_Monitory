5b4450f5a56831bb616ccf4aeb50a03113ced956
Botnets <julio.gaviria@correo.icesi.edu.co>
2020-02-08 11:49:50 -0500

    Creation of example in vue of a simple get request with a public API

---
+++ tools/Vue.js/Example-get-Request-API/vue-request-guet-example/vue-request-guet-ex/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <v-card class="overflow-hidden">
+      <v-app-bar
+        absolute
+        color="#43a047"
+        dark
+        shrink-on-scroll
+        prominent
+        src="https://picsum.photos/1920/1080?random"
+        fade-img-on-scroll
+        scroll-target="#scrolling-techniques-5"
+        scroll-threshold="500"
+      >
+        <template v-slot:img="{ props }">
+          <v-img v-bind="props" gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"></v-img>
+        </template>
+
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Vuetify Request Get with VUE</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-sheet id="scrolling-techniques-5" class="overflow-y-auto" max-height="600">
+        <v-container style="height: 150px;"></v-container>
+      </v-sheet>
+      <div class="container">
+        <img src="./assets/logo.png" />
+        <div class="call-button-container">
+          <v-btn @click="consumeApi()">GET request from API</v-btn>
+        </div>
+        <v-divider></v-divider>
+        <div class="margin">
+            <p>{{ info }}</p>
+        </div>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  data() {
+    return {
+      info: null
+    };
+  },
+  methods: {
+    consumeApi() {
+      axios
+        .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        .then(response => (this.info = response));
+    }
+  }
+};
+</script>
+
+<style>
+
+.margin {
+  margin-top: 20px;
+}
+</style>
